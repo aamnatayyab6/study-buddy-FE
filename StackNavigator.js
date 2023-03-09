cdd316968ab7d5b421de84f849da502e84555a4e
aamnatayyab6 <aamnatayyab6@yahoo.com>
2023-03-09 23:03:29 +0100

    implementing useAuth for logged in user

--- StackNavigator.js
+++ StackNavigator.js
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import useAuth from "./hooks/useAuth";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import MessagesScreen from "./screens/MessagesScreen";
@@ -7,13 +8,12 @@ import MessagesScreen from "./screens/MessagesScreen";
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-    const user = true;
+    const {user} = useAuth();
 
     return (
         <Stack.Navigator>
             {user ? (
                 <>
-                    <Stack.Screen name="Login" component={LoginScreen}/>
                     <Stack.Screen name="Home" component={HomeScreen}/>
                     <Stack.Screen name="Messages" component={MessagesScreen}/>
                 </>
